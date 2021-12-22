39b12964806be6c3104bf2975cb8a838153a17e5
Alex <maroiualex@gmail.com>
2021-12-22 20:09:09 +0200

    Initial commit

---
+++ src/main/java/com/ComenziRestaurant/demo/ComenziRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.ComenziRestaurant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComenziRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComenziRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ComenziRestaurant/demo/ComenziRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ComenziRestaurant.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComenziRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
